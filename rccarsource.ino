231e5a178770f097301b9421fa3a040f71a8967b
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-08 11:12:35 +0900

    Update rccarsource.ino

--- rccarsource.ino
+++ rccarsource.ino
@@ -32,24 +32,14 @@ void loop() {
      value = mySerial.read(); //전송값 읽음
      Serial.print(value);
      
-     if(value == 'l'){            //좌회전(강)
-       Serial.println("left");
-       left();
-     }else if(value == 'r'){      //우회전(강)
-       Serial.println("right");
-       right();
-     }else if(value == 'f'){      //전진
+
+     if(value == 'f'){      //전진
        Serial.println("forward");
        forward();
      }else if(value == 'b'){      //후진
        Serial.println("backward");
        backward();
-     }else if(value == 'w'){      //우회전(약)
-       Serial.println("slow right");
-       sRight();
-     }else if(value == 'q'){      //좌회전(약)
-       Serial.println("slow left");
-       sLeft();       
+     }       
      }else if(value == 's'){      //정지
        Serial.println("stop");    
        stop(); 
@@ -70,30 +60,6 @@ void backward(){
   digitalWrite(LEFT_B1, HIGH);
   digitalWrite(RIGHT_A2, LOW);
   digitalWrite(RIGHT_B2, HIGH);
-}
-void left(){
-  digitalWrite(LEFT_A1, LOW);
-  digitalWrite(LEFT_B1, HIGH);
-  digitalWrite(RIGHT_A2, HIGH);
-  digitalWrite(RIGHT_B2, LOW);
-}
-void right(){
-  digitalWrite(LEFT_A1, HIGH);
-  digitalWrite(LEFT_B1, LOW);
-  digitalWrite(RIGHT_A2, LOW);
-  digitalWrite(RIGHT_B2, HIGH);
-}
-void sLeft(){
-  digitalWrite(LEFT_A1, LOW);
-  digitalWrite(LEFT_B1, LOW);
-  digitalWrite(RIGHT_A2, HIGH);
-  digitalWrite(RIGHT_B2, LOW);
-}
-void sRight(){
-  digitalWrite(LEFT_A1, HIGH);
-  digitalWrite(LEFT_B1, LOW);
-  digitalWrite(RIGHT_A2, LOW);
-  digitalWrite(RIGHT_B2, LOW);
 }
 void stop(){
   digitalWrite(LEFT_A1, LOW);
