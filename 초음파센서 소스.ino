c6e92f33c399e30e09bb7af285d59016c6d2314e
2013136082_R-pet'sSafeHabitat <kutmicro@gmail.com>
2016-11-10 11:17:58 +0900

    Update 초음파센서 소스.ino

--- 초음파센서 소스.ino
+++ 초음파센서 소스.ino
@@ -23,16 +23,6 @@ void loop()
   delay(300);  // 0.3초 대기 후 다시 측정
 }
 
-long microsecondsToInches(long microseconds)
-{
-  // According to Parallax's datasheet for the PING))), there are
-  // 73.746 microseconds per inch (i.e. sound travels at 1130 feet per
-  // second).  This gives the distance travelled by the ping, outbound
-  // and return, so we divide by 2 to get the distance of the obstacle.
-  // See: http://www.parallax.com/dl/docs/prod/acc/28015-PING-v1.3.pdf
-  return microseconds / 74 / 2;
-}
-
 long microsecondsToCentimeters(long microseconds)
 {
   // The speed of sound is 340 m/s or 29 microseconds per centimeter.
